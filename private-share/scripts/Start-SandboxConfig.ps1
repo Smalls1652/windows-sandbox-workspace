6cc9ee08d62345a82a8f78506101196680e2a7b2
Timothy Small <smalls.online@icloud.com>
2020-12-04 10:53:24 -0500

    Added Sysmon install to the startup script

--- private-share/scripts/Start-SandboxConfig.ps1
+++ private-share/scripts/Start-SandboxConfig.ps1
@@ -9,7 +9,8 @@ $scriptPath = $PSScriptRoot
 
 $scriptsToRun = @(
     [System.IO.Path]::Combine($scriptPath, "Install-EdgeEnterprise.ps1"),
-    [System.IO.Path]::Combine($scriptPath, "Configure-EdgeSettings.ps1")
+    [System.IO.Path]::Combine($scriptPath, "Configure-EdgeSettings.ps1"),
+    [System.IO.Path]::Combine($scriptPath, "Install-Sysmon.ps1")
 )
 
 foreach ($scriptFile in $scriptsToRun) {
